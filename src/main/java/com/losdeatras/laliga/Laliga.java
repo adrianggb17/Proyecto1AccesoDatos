4f1af6cf38a273cb0b4f9048be09e0db00ad20a3
Adrian17 <adrian172001ggb@gmail.com>
2023-09-27 15:57:27 +0200

    Creo el proyecto y lo subo a GiT

---
+++ src/main/java/com/losdeatras/laliga/Laliga.java
@@ -0,0 +1,12 @@
+package com.losdeatras.laliga;
+
+/**
+ *
+ * @author Adrian
+ */
+public class Laliga {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
