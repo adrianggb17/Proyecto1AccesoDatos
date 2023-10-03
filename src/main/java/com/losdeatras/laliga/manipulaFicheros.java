3efe03e8f0e853d1d427ddfda797de1a68dff13a
Miguel Herreros Pinilla <75880177+miguel9912@users.noreply.github.com>
2023-10-03 20:57:01 +0200

    Add files via upload

---
+++ src/main/java/com/losdeatras/laliga/manipulaFicheros.java
@@ -0,0 +1,53 @@
+package com.losdeatras.laliga;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ *
+ * @author Miguel Herreros Pinilla
+ */
+public class manipulaFicheros {
+    String nombre;
+    
+    /**
+     * @author Miguel Herreros Pinilla
+     * @param nombre
+     * @param tipo
+     * @return
+     * @throws IOException 
+     */
+    public boolean crearFichero(String nombre, int tipo)throws IOException{
+        File f;
+        switch (tipo) {
+            case 1:
+                f = new File(nombre+".txt");
+                break;
+            case 2:
+                f = new File(nombre+".dat");
+            default:
+                throw new AssertionError();
+        }
+                    return f.createNewFile();
+
+               
+    }
+    
+    public void moverFichero(File f, File ruta) throws IOException{
+        if(f.exists()){
+            File fAux = f;
+        if(ruta.exists()){
+            String route = f.getAbsolutePath();
+            f = new File(f,route);
+            f.createNewFile();
+        }else{
+            System.out.println("La ruta indicada no existe");
+        }
+        }
+        
+    }
+    
+    
+    
+    
+}
