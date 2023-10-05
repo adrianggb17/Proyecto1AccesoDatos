1fc269fb271679388aeab2586e953c666a40247c
Miguel Herreros Pinilla <mhpcalatrava@gmail.com>
2023-10-05 20:09:09 +0200

    Hemos avanzado en la creación de archivos

--- src/main/java/com/losdeatras/laliga/crearFicherosTexto.java
+++ src/main/java/com/losdeatras/laliga/crearFicherosTexto.java
@@ -8,37 +8,70 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 /**
  *
  * @author Alumno
  */
 public class crearFicherosTexto {
+final static int LONGITUD_NOMBRE = 100;
     final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    public static void main(String[]args){
+
+    public static void main(String[] args) {
         BufferedWriter bfw = null;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String ruta = "";
-        File f =  new File(ruta);
-        int id=1;
-        String nombre="";
-        int puntos=0;
-        int liga=0;
-        
-        try{
+        Scanner sc = new Scanner(System.in);
+        try {
+            //Introducimos el ID
+            System.out.println("Introduce el ID del equipo");
+            int id = br.read();
+            br.readLine();
+            //Introducimos el nombre
+            System.out.println("Introduce el nombre del equipo");
+            String nombre = br.readLine();
+            System.out.println("Me suda la polla esto");
+            //Creamos la ruta del archivo
+            String ruta = nombre + ".txt";
+            //Introducimos los puntos que ha conseguido el equipo
+            System.out.println("Introduce la cantidad de puntos obtenida por el "+nombre);
+            int puntos = br.read();
+            //Introducimos la liga en la que juega
+            System.out.println("¿Este equipo juega en la liga 1 o en la liga 2?");
+            int liga = -1;
+            do{
+            liga = br.read();
+            }while(liga !=1 && liga !=2);
+            File f = new File(ruta);
+
             bfw = new BufferedWriter(new FileWriter(ruta, StandardCharsets.UTF_8));
-            if(f.createNewFile()){
+            if (!f.createNewFile()) {
+                System.out.println("Ha habido un problema al crear el fichero");
+            }else{
                 System.out.println("Fichero creado correctamente");
+                //INTRODUCIMOS EL ID
+                bfw.write(id);
+                //INTRODUCIMOS EL NOMBRE CARACTER A CARACTER CON UN TAMAÑO PREFIJADO
+                for (int i = 0; i < LONGITUD_NOMBRE; i++) {
+                    if(i<nombre.length()){
+                        bfw.write(nombre.charAt(i));
+                    }
+                    else{
+                    bfw.write(' ');
+                }
+                }
+                //INTRODUCIMOS LOS PUNTOS
+                bfw.write(puntos);
+                //INTRODUCIMOS LA LIGA EN LA QUE SE ENCUENTRA
+                bfw.write(liga);
             }
-            bfw.write(id);
-            bfw.write(nombre);//Esto va aparte
-            bfw.write(puntos);
-            bfw.write(liga);
             
-        }catch (FileNotFoundException fn) {
+
+        } catch (FileNotFoundException fn) {
             System.out.println("No se encuentra el fichero en la ruta indicada");
         } catch (IOException io) {
             System.out.println("Error de E/S");
         }
     }
+    
 }
