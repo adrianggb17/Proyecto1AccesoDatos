a18aa2ffddb14ab446ad5039d9ed44d008624462
Adrian17 <adrian172001ggb@gmail.com>
2023-10-11 16:14:19 +0200

    Subo la parte de crear DOM y lectura del XML de entrenadores con DOM

---
+++ src/main/java/com/losdeatras/laliga/CrearDocumentoDOM.java
@@ -0,0 +1,86 @@
+package com.losdeatras.laliga;
+
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.DOMException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Adrian
+ */
+public class CrearDocumentoDOM {
+    
+    private static final Properties myProperties = new Properties();
+    
+    private static final String ENTRENADOR = "ENTRENADOR";
+    private static final String IDENTRENADOR = "IDENTRENADOR";
+    private static final String NOMBREENTRENADOR = "NOMBRENTREN";
+    private static final String EDAD = "EDAD";
+    
+        public static void main(String[] args) throws Exception {
+
+        try {
+            CrearDocumentoDOM.myProperties.load(new FileInputStream("libros_prj.properties"));
+        } catch (IOException e) {
+            System.out.println("No se pueden cargar la inicialización del programa. Saliendo...");
+            System.exit(100);
+        }
+
+        String fichero_original = CrearDocumentoDOM.myProperties.getProperty("xml_path.old");
+
+        Document doc = null;
+        try {
+            DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+            doc = docBuilder.newDocument();
+            Element rootElement = doc.createElement(ENTRENADOR);
+            doc.appendChild(rootElement);
+
+            Element entrenador;
+            entrenador = CrearDocumentoDOM.createNodeEntrenador(doc, new Entrenador("060", "Jose", "50"));
+            rootElement.appendChild(entrenador);
+
+            
+        } catch (ParserConfigurationException | DOMException e) {
+            System.out.println(e.getLocalizedMessage());
+            throw e;
+        }
+    }
+     private static Element createNodeEntrenador(Document doc, Entrenador entrenador) throws DOMException {
+        // nodo donde colgaremos todos los datos de un libro
+        Element nodeEntrenador = null;
+        try {
+            nodeEntrenador = doc.createElement(ENTRENADOR);
+
+            Element identre = doc.createElement(IDENTRENADOR);
+            identre.appendChild(doc.createTextNode(entrenador.getIDENTRENADOR()));
+            nodeEntrenador.appendChild(identre);
+            
+            Element nombreEntre = doc.createElement(NOMBREENTRENADOR);
+            nombreEntre.appendChild(doc.createTextNode(entrenador.getNOMBREENTRENADOR()));
+            nodeEntrenador.appendChild(nombreEntre);
+            
+            Element edad = doc.createElement(EDAD);
+            edad.appendChild(doc.createTextNode(entrenador.getEDAD()));
+            nodeEntrenador.appendChild(edad);
+
+        } catch (DOMException e) {
+            System.out.println(e.getLocalizedMessage());
+            throw e;
+        }
+
+        return nodeEntrenador;
+    }
+}
---
+++ src/main/java/com/losdeatras/laliga/LecturaXmlDOM.java
@@ -0,0 +1,72 @@
+package com.losdeatras.laliga;
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import javax.swing.text.Document;
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import org.w3c.dom.DOMException;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Adrian
+ */
+public class LecturaXmlDOM {
+    
+    private static final Properties myProperties = new Properties();
+
+    //Declaro las constantes de mi xlm
+    private static final String ENTRENADOR = "ENTRENADOR";
+    private static final String IDENTRENADOR = "IDENTRENADOR";
+    private static final String NOMBREENTRENADOR = "NOMBRENTREN";
+    private static final String EDAD = "EDAD";
+    
+     public static void main(String[] args) {
+        // Recuperación del archivo de propiedades para su posterior trabajo
+        try {
+            LecturaXmlDOM.myProperties.load(new FileInputStream("entrenadores_prj.properties"));
+            
+            org.w3c.dom.Document doc = null; 
+
+            System.out.println("Recuperación de la información de los entrenadores");
+            
+            Node raiz = doc.getFirstChild();
+            NodeList listaNodosEntrenadores = raiz.getChildNodes();
+
+            for (int i = 0; i < listaNodosEntrenadores.getLength(); i++) {
+                
+                Node nodoEntrenador = listaNodosEntrenadores.item(i);
+                if (nodoEntrenador != null && nodoEntrenador.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eEntrenador = (Element) nodoEntrenador;
+                    eEntrenador.getAttribute(LecturaXmlDOM.IDENTRENADOR);
+                    eEntrenador.getElementsByTagName(LecturaXmlDOM.NOMBREENTRENADOR).item(0).getTextContent();
+                    eEntrenador.getElementsByTagName(LecturaXmlDOM.EDAD).item(0).getTextContent();
+
+                    Entrenador entrenador = new Entrenador (IDENTRENADOR, NOMBREENTRENADOR, EDAD);
+                    System.out.println(entrenador);
+                    }
+                }
+        } catch (IOException e) {
+            System.out.println("No se pueden cargar la inicialización del programa. Saliendo...");
+            System.exit(100);
+             
+        }
+        
+        String fichero_original = LecturaXmlDOM.myProperties.getProperty("xml_path.old");
+        }    
+}   
