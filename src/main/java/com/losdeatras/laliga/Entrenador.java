7e2a810cb71f18d5b5f7d28c729cb4e427f77a37
Alvaro <Alvaro@DESKTOP-DJ0RN19>
2023-10-11 11:30:55 +0200

    subo anotaciones de la clase entrenador

--- src/main/java/com/losdeatras/laliga/Entrenador.java
+++ src/main/java/com/losdeatras/laliga/Entrenador.java
@@ -1,14 +1,23 @@
 
 package com.losdeatras.laliga;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
 /**
  *
  * @author Alvaro
  */
+
+@XmlRootElement
 public class Entrenador {
     
+    @XmlElement(name = "IDENTRENADOR", required = true)
     private  int idEntrenador;
+    @XmlElement(name = "NOMBRENTREN", required = true)
     private String nombre;
+    @XmlElement(name = "EDAD", required = true)
     private int edad;
 
     public Entrenador(int idEntrenador, String nombre, int edad) {
@@ -16,27 +25,30 @@ public class Entrenador {
         this.nombre = nombre;
         this.edad = edad;
     }
-
+    
+    @XmlTransient
     public int getIdEntrenador() {
         return idEntrenador;
     }
-
+    
+     public void setIdEntrenador(int idEntrenador) {
+        this.idEntrenador = idEntrenador;
+    }
+    
+    @XmlTransient
     public String getNombre() {
         return nombre;
     }
-
-    public int getEdad() {
-        return edad;
-    }
-
-    public void setIdEntrenador(int idEntrenador) {
-        this.idEntrenador = idEntrenador;
-    }
-
+    
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
+    
+    @XmlTransient
+    public int getEdad() {
+        return edad;
+    }
+    
     public void setEdad(int edad) {
         this.edad = edad;
     }
